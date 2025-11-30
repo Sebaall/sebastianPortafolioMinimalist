82da065019049a3f41954e853fbaac6c39f807a8
{Seba} <sebaleonpr@gmail.com>
2025-11-29 22:45:57 -0300

    "sidebar"

--- js/slider.js
+++ js/slider.js
@@ -60,3 +60,9 @@ document.getElementById("menu-btn").addEventListener("click", () => {
     const menu = document.getElementById("mobile-menu");
     menu.classList.toggle("hidden");
 });
+
+document.querySelectorAll("#mobile-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("mobile-menu").classList.add("hidden");
+    });
+});
